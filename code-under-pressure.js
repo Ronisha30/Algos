65311da02aeffa4c46a23d2d460ba0298acbc75e
Ronisha30 <ronisha.isham@yahoo.com>
2021-11-22 19:00:24 -0600

    code-under-pressure

---
+++ code-under-pressure.js
@@ -0,0 +1,6 @@
+//https:www.codewars.com/kata/53ee5429ba190077850011d4/solutions/javascript/me/best_practice
+// i will be an integer. Double it and return it.
+function doubleInteger(i) {
+
+    return i * 2;
+  }
