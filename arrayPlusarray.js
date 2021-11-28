b34e70f9daff81da61921c50180e5fd3073e62a7
Ronisha30 <ronisha.isham@yahoo.com>
2021-11-27 20:04:48 -0600

    opposite number

--- arrayPlusarray.js
+++ arrayPlusarray.js
@@ -1,3 +1,5 @@
+//https:www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
+// code as fast as you can! You need to double the integer and return it 
 function arrayPlusArray(arr1, arr2) {
     let s = 0;
     let arr = [...arr1, ...arr2]
---
+++ opposite-number.js
@@ -0,0 +1,7 @@
+//www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
+// ruturned the number
+
+function opposite(num) {
+ 
+    return num *= -1;
+  }
