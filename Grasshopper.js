a0373a1d34fa875de5de7b6be488df4e701254d5
Ronisha30 <ronisha.isham@yahoo.com>
2021-11-27 23:55:56 -0600

    Grasshopper Summation

--- Grasshopper.js
+++ Grasshopper.js
@@ -1,3 +1,6 @@
+// Create a for loop
+// Return the sum
+// www.codewars.com/kata/55d24f55d7dd296eb9000030/train/javascript
 let summation = function (num) {
     let sum = 0
     for(let i = 0; i <= num; i++) {
