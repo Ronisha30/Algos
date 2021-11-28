be7a62ff4f226721894768e1d46d8721ab23b9a1
Ronisha30 <ronisha.isham@yahoo.com>
2021-11-27 23:47:54 -0600

    Can We Divide It

---
+++ Can-We-Divide-It.js
@@ -0,0 +1,9 @@
+// Create a two variables
+// Use to the moduls to divide
+// Use the strictly equal operator
+// use the logical operator
+// return both a and b
+//www.codewars.com/kata/5a2b703dc5e2845c0900005a/train/javascript
+function isDivideBy(number, a, b) {
+    return (number % a === 0) && (number % b === 0);
+  }
