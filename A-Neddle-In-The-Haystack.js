b80d4f7b780c6fa8332bff99259971f1ccc6b90a
Ronisha30 <ronisha.isham@yahoo.com>
2021-11-29 00:14:09 -0600

    A needle in the Haystack

---
+++ A-Neddle-In-The-Haystack.js
@@ -0,0 +1,7 @@
+// Use indexof method
+// return the postion
+//www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/javascript
+function findNeedle(haystack) {
+    return 'found the needle at position ' + haystack.indexOf('needle');
+  }
+  
