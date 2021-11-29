77aa363ea55d28277c26e808ee2172a470795e00
Ronisha30 <ronisha.isham@yahoo.com>
2021-11-28 23:17:05 -0600

    Keep Hydrated

--- Keep-Hydrated.js
+++ Keep-Hydrated.js
@@ -2,4 +2,4 @@
 //Returns the value  
 //www.codewars.com/kata/582cb0224e56e068d800003c/train/javascript
 function litres(time) {
-    return Math.floor(time*0.5);
+    return Math.floor(time*0.5);}
