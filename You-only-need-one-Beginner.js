a455c9dc2147a249902aa698c9cd922bf7bc63ed
Ronisha30 <ronisha.isham@yahoo.com>
2021-12-02 12:59:24 -0600

    You only need one

---
+++ You-only-need-one-Beginner.js
@@ -0,0 +1,15 @@
+//www.codewars.com/kata/57cc975ed542d3148f00015b
+// checking a (array) to see x  my (value) is inside it
+// Return true or false
+function check(a, x) {
+    let result = false
+    for(let i = 0; i < a.length; i ++){
+      if(a[i] == x){
+        result = true
+      
+      } 
+        
+      }
+    return result
+    } 
+    
