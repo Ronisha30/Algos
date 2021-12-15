295dd36270d01c77b70df6a86bd8633561daa704
Ronisha30 <ronisha.isham@yahoo.com>
2021-12-14 18:04:42 -0600

    Vowel Count

--- Sum-positive.js
+++ Sum-positive.js
@@ -2,7 +2,7 @@
 // if arr[i] is greater than zero
 // add arr[i] to total
 // return total
-https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript
+//www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript
 function positiveSum(arr) {
     let sum = 0 
     for(let i = 0; i < arr.length; i++){
---
+++ Vowel-Count.js
@@ -0,0 +1,16 @@
+//Create a variable to get the count of the vowel
+//Use a loop to go over the arr
+//Create an array
+//Return the count
+//www.codewars.com/kata/54ff3102c1bad923760001f3/train/javascript
+function getCount(str) {
+    let vowelsCount = 0;
+    let arr = str.split("")
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i]=== 'a'|| arr[i]==='e'|| arr[i]==='i'||  arr[i]==='o'||arr[i]==='u'){
+        vowelsCount += 1
+      }
+    }
+    
+    return vowelsCount;
+  }
