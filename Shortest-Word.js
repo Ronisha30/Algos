5a5748e2c824736f271c6c2f638ac52e3ce4b18a
Ronisha30 <ronisha.isham@yahoo.com>
2022-01-03 13:06:19 -0600

    Final Grade

--- Shortest-Word.js
+++ Shortest-Word.js
@@ -1,14 +1,23 @@
-function findShort(s){
-    let words = s.split(" ");
-    let shortLength = words[0].length
-    console.log(words)
-    for (let i = 0; i < words.length; i++){
-    console.log(words[i])
-    if (shortLength > words[i].length){
-    shortLength = words[i].length
-    console.log (words[i], "updated short")
-    }
-    }
-    return shortLength;
-    }
+// function findShort(s){
+//     let words = s.split(" ");
+//     let shortLength = words[0].length
+//     console.log(words)
+//     for (let i = 0; i < words.length; i++){
+//     console.log(words[i])
+//     if (shortLength > words[i].length){
+//     shortLength = words[i].length
+//     console.log (words[i], "updated short")
+//     }
+//     }
+//     return shortLength;
+//     }
+    function solution(number){
+        let output = 0; 
+        for(let i = 0; i < number.length; i++){
+            let currNum = [i]
+            output += currNum;
+        
+        }
+        return output
+        }
     
