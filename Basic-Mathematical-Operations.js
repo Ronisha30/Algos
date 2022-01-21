86c9eb7e1c57d42b5b84e91d95e806dba2fa47ec
Ronisha30 <ronisha.isham@yahoo.com>
2022-01-21 12:28:26 -0600

    Basic Math Operations

---
+++ Basic-Mathematical-Operations.js
@@ -0,0 +1,13 @@
+function basicOp(operation, value1, value2)
+// Check if operation truly equals to value 1 and value 2
+// Return the results of values
+{
+if(operation === '+'){
+  return value1 + value2
+} else if (operation === '-'){
+  return  value1 - value2 
+} else if (operation === '*'){
+  return value1 * value2
+}else (operation === value1/value2)
+  return value1/value2
+}
