318330a10b30e0534c0953eb2225ceca28cb5767
Ronisha30 <ronisha.isham@yahoo.com>
2022-02-04 09:22:36 -0600

    ALT CAP

---
+++ Alternate- Cap.js
@@ -0,0 +1,4 @@
+function capitalize(s){
+    return [s.split('').map((v,i)=>i%2==0?v.toUpperCase():v.toLowerCase()).join(''),
+            s.split('').map((v,i)=>i%2!==0?v.toUpperCase():v.toLowerCase()).join('')]
+  };
