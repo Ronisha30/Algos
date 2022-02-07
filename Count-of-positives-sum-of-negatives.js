904a9418d1b5ef9dee8a6c2e00e426142fb37e87
Ronisha30 <ronisha.isham@yahoo.com>
2022-02-06 23:49:20 -0600

    Count of positives and negatives

---
+++ Count-of-positives-sum-of-negatives.js
@@ -0,0 +1,10 @@
+function countPositivesSumNegatives(input) {
+    let positiveNums = 0;
+    let negativeNums = 0;
+    if (input === null || input.length === 0) {
+      return [];
+    } else {
+      input.forEach((num) => num > 0 ? positiveNums++ : negativeNums += num);
+    }
+    return [positiveNums , negativeNums];
+}
