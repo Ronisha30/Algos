32ae7ecf5bb9cc06dbe7a4a4967ec034be82e0e0
Ronisha30 <ronisha.isham@yahoo.com>
2022-04-29 17:19:26 -0500

    Caluclate Average

---
+++ Calculate-Average.js
@@ -0,0 +1,13 @@
+// Find the average of the numbers inside of the array 
+function find_average(array) {
+    // Decleared a variable called total
+    let total = 0;
+    // Created for loop to fid the average of the array
+    for(let i = 0; i < array.length; i++){
+        // Added to the array
+      total += array[i]
+      }
+      // Returned the average of the array
+      return(total/array.length)
+    }
+    
