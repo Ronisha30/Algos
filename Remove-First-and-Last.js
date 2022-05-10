7421256b91a12a56f533db6f6a3885d71b370c71
Ronisha30 <ronisha.isham@yahoo.com>
2022-05-09 23:55:17 -0500

    Remove First and Last Character

---
+++ Remove-First-and-Last.js
@@ -0,0 +1,12 @@
+function removeChar(str){
+    // input: string
+    // output: Return the first and last chars of my string
+    
+      // Declear a veriable set to empty string 
+    let outpuut = "";
+      // Use the sustring method to get the first and last of the chars indexes 
+    let results = str.substring(1, str.length -1);
+    // Return the string results
+      return results;
+    };
+    
