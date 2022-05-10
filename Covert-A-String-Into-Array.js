033f6cebe59f5cbe0d6cec03a27f8f4b2fa21e2a
Ronisha30 <ronisha.isham@yahoo.com>
2022-05-10 09:06:49 -0500

    Converted a string into an array

---
+++ Covert-A-String-Into-Array.js
@@ -0,0 +1,10 @@
+// Input: a empty string
+//Output: String converted to a array
+function stringToArray(string) {
+  // Set output variable to an empty string
+  let output = "";
+  // Used a string as a sperator for each word
+  let arr = string.split(" ");
+  // Return that array as the new aray
+  return arr;
+}
