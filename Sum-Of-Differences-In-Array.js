1542dbaa9519a321ea42f51b5c05829865a84ddf
Ronisha30 <ronisha.isham@yahoo.com>
2022-05-21 23:48:14 -0500

    Sum-Of-Differences-Array

---
+++ Sum-Of-Differences-In-Array.js
@@ -0,0 +1,16 @@
+function sumOfDifferences(arr) {
+    // input: array
+    // output: The sum of consecutive value pairs in an arr
+  
+    // I will use the .sort() function I wnt my array to go in descending order
+    const sorted = arr.sort((a, b) => b - a);
+    // Declear a variable called sum
+    let sum = 0;
+    // Use a for loop to find all of the consecutive number value pairs
+    for (let i = 1; i < sorted.length; i++) {
+      sum += sorted[i - 1] - sorted[i];
+    }
+    // Reurn the sum of the sorted array
+    return sum;
+  }
+  
