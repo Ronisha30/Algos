a70414c81abdeeb9b13dcc10b30714818a295954
Ronisha30 <ronisha.isham@yahoo.com>
2022-05-22 23:53:32 -0500

    Reverse Words

---
+++ Reverse-Words-Level-8.js
@@ -0,0 +1,9 @@
+//input: string
+//output: string
+//reverses all of the words in the string 
+// Use the .split(), .reverse(), and .join() methods
+function reverseWords(str){
+    //return string
+  return str.split(' ').reverse().join(' ')
+  }
+  
